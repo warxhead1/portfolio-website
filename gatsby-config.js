4d6cd02ae3e950795109ba86543fa8f415006179
warxhead1 <warxhead1@users.noreply.github.com>
2021-11-07 14:04:29 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
+    siteTitle: `Dietrich Adventures`,
+    siteTitleAlt: `Dietrich Adventures I.T. Specialist`,
+    siteHeadline: `Eric's Portfolio`,
+    siteUrl: `https://dietrichadventures.com`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@eric`,
   },
   plugins: [
     {
@@ -25,9 +25,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Eric - Portfolio`,
+        short_name: `Eric`,
+        description: `Expanding my clientele reach by showcasing my portfolio`,
         start_url: `/`,
         background_color: `#141821`,
         // This will impact how browsers show your PWA/website
